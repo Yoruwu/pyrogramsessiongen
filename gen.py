d278a839257b6482e8b978edb660c60c99a9a87b
Yet Another Idiot <127736548+Yoruwu@users.noreply.github.com>
2024-06-02 22:12:29 +0200

    Update gen.py
    fixed wording and adding info on where to get ID & Hash

--- gen.py
+++ gen.py
@@ -27,15 +27,16 @@ def print_line3(text):
 
 # prints banner
 print_line1("\nPyrogram Session String Generator\n")
-print_line2("\nVERSION: Alpha - 0.0.2\n")
+print_line2("\nVERSION: Alpha - 0.0.3\n")
 print_line3("\nCREATED BY: @Yoruwu\n")
+print(f"\nYou can get ID and Hash on the official telegram website.\n")
 
 #input filds for api id and hash
 API_ID = input("\nEnter Your API_ID:\n > ")
 API_HASH = input("\nEnter Your API_HASH:\n > ")
 
 # prints instructions for pyrogram auth
-print("\n\n Enter Phone number when asked.\n\n")
+print("\n\n Enter Telegram Phone number when asked.\n\n")
 
 i = c(":memory:", api_id=API_ID, api_hash=API_HASH)
 
@@ -43,7 +44,7 @@ i = c(":memory:", api_id=API_ID, api_hash=API_HASH)
 async def main():
 	await i.start()
 	ss = await i.export_session_string()
-	print("\nHERE IS YOUR STRING SESSION, COPY IT, DON'T SHARE!!\n")
+	print("\nHERE IS YOUR SESSION STRING/ID, COPY IT, DON'T SHARE!!\n")
 	print(f"\n{ss}\n")
 
 
